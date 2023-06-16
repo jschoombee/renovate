c898e59f293247f66e51c6febad7179feda79278
jschoombee <jp.schoombee@gmail.com>
2023-06-16 11:18:20 +0200

    with debug

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     platform: 'github',
     gitAuthor: 'renovate-bot <renovate@jpson>',
-    enabledManagers: [ 'regex' ],
+    enabledManagers: [ 'regex', 'terraform' ],
     logLevel: 'debug',
     token: process.env.GITHUB_TOKEN,
     dependencyDashboardTitle: '[renovate-bot] Update Dependencies',
