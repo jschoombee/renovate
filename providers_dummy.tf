66730b3812b22f23cb4376df16f340aedffc70c4
jschoombee <jp.schoombee@gmail.com>
2023-06-26 14:34:48 +0200

    p

--- providers_dummy.tf
+++ providers_dummy.tf
@@ -5,17 +5,17 @@ terraform {
     wiz = {
       source  = "axtongrams/wiz"
       version = "1.1.3" # renovate: depName=wiz-dummy-module
-      meta = {
-        depName = "wiz-dummy"
-      }
+      # meta = {
+      #   depName = "wiz-dummy"
+      # }
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "= 2.26.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "= 3.41.0"
-    }
+    # azuread = {
+    #   source  = "hashicorp/azuread"
+    #   version = "= 2.26.0"
+    # }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "= 3.41.0"
+    # }
   }
 }
